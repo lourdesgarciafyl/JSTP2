d92f2d0ef07b593c678396ebb4c136f7f7822e66
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-03-07 09:46:39 -0300

    corrección para chequear si el dato ingresado es un numero

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -5,16 +5,13 @@ no es un número válido indicarlo en un mensaje.*/
 
 let edad = parseInt(prompt("Ingresá tu edad para saber si podes conducir"));
 
-switch(true){
-    case ((edad > 18) && (edad <= 100) ): 
-    document.write("<p>Ya podes conducir</p>");
-    break;
-    case (edad <= 18):
-    document.write("<p>No podes conducir todavía</p>");
-    break;
-    case (edad > 100):
-    document.write("<p>Ingresá un numero válido por favor</p>");
-    break;
-    default:
-    document.write("Ingresa un numero por favor");
+if (!isNaN(edad) && edad >= 0 && edad <= 100){
+    if(edad >= 18){
+        document.write("<p>Puedes conducir</p>");
+    } 
+    else{
+        document.write("<p>Aún no puedes conducir</p>");
+    }
+}else{
+    alert("Ingrese un número válido");
 }
