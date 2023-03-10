05f1339a63d954dacac73afed2d0bf2d9741079b
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-03-10 08:49:10 -0300

    se crea la tabla con html y le damos estilo con css

---
+++ js/ejercicio10.js
@@ -0,0 +1,20 @@
+/* Realiza un script que pida número de filas y columnas
+ y escriba una tabla. Dentro de cada una de las celdas 
+ deberá escribirse un número consecutivo en orden 
+ descendente. Si, por ejemplo, la tabla es de 7×5 
+ los números irán del 35 al 1. */
+
+ let filas = parseInt(prompt("Ingrese un numero de filas"));
+ let columnas = parseInt(prompt("Ingrese un numero de columnas"));
+
+ if(!isNaN){
+
+ } document.write(
+`<table>
+ <tbody>
+     <tr>
+         <td> 1 </td>
+         <td> 2 </td>
+     </tr>
+ </tbody>
+</table>`)
