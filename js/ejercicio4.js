584cd283364cb63182ce10965a92c53d5cdc90f9
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-03-10 16:26:49 -0300

    resalto palabras importantes de las consignas, para destacar las que ayudan a saber cuando usar if, switch, for, do, do while

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -1,9 +1,8 @@
-/* Realiza un script que pida números hasta que 
+/* Realiza un script que PIDA números HASTA QUE 
 se pulse “cancelar”. Si no es un número deberá 
 indicarse con un «alert» y seguir pidiendo números. 
 Al salir con “cancelar” deberá indicarse la suma total 
-de los números introducidos.
-*/
+de los números introducidos.*/
 let acumulador = 0;
 let numero = 0;
 do{
--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -2,7 +2,7 @@
 // El algoritmo para calcular la letra del dni es el siguiente :
 // El número debe ser entre 0 y 99999999
 // Debemos calcular el resto de la división entera entre el número y el número 23.
-// Según el resultado, de 0 a 22, le corresponderá una letra de las siguientes:  (T, R, W, A, G, M, Y, F, P, D, X, B, N, J, Z, S, Q, V, H, L, C, K, E)
+// SEGÚN el resultado, de 0 a 22, le corresponderá una letra de las siguientes:  (T, R, W, A, G, M, Y, F, P, D, X, B, N, J, Z, S, Q, V, H, L, C, K, E)
 // Si lo introducido no es un número deberá indicarse con un alert y volver a preguntar.
 // Deberá de REPETIRSE el proceso HASTA QUE el usuario pulse «cancelar».
 
